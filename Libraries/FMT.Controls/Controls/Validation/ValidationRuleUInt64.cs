126ae46b094377e29e84c283e87a6f31980cef08
Paulov <pargsoft@hotmail.com>
2023-07-25 14:46:06 +0100

    Support ULong/UInt64 EBX Editing

---
+++ Libraries/FMT.Controls/Controls/Validation/ValidationRuleUInt64.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace FMT.Controls.Validation
+{ 
+    internal class ValidationRuleUInt64 : ValidationRule
+    {
+        public ValidationRuleUInt64()
+        {
+        }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            return ulong.TryParse(value.ToString(), out _) ? ValidationResult.ValidResult : new ValidationResult(false, "Input is not a valid UInt64/ULong");
+        }
+    }
+}
