d08d23f38f7d2c557e58aa365f686fa6aeb02dd2
Paulov <pargsoft@hotmail.com>
2023-08-12 15:54:18 +0100

    New Feature "LocaleIniEditor string finder" - Stub

--- FrostbiteModdingUI/Windows/LocaleINIEditor.xaml.cs
+++ FrostbiteModdingUI/Windows/LocaleINIEditor.xaml.cs
@@ -1,8 +1,17 @@
-﻿using FrostySdk.Managers;
+﻿using FIFAModdingUI.Windows.Profile;
+using FMT.FileTools;
+using FrostySdk;
+using FrostySdk.FrostbiteSdk.Managers;
+using FrostySdk.Managers;
 using MahApps.Metro.Controls;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Documents;
 
 namespace FMT.Windows
 {
@@ -16,9 +25,13 @@ namespace FMT.Windows
             InitializeComponent();
             var d = Data;
 
+            Directory.CreateDirectory(CachedEXEStringDirectoryPath);
+
             this.DataContext = null;
             this.DataContext = this;
         }
+
+        #region Properties
 
         private string data;
         public string Data
@@ -40,11 +53,53 @@ namespace FMT.Windows
             }
         }
 
-        protected override void OnContentRendered(EventArgs e)
-        {
+        public static string[] EXEStrings { get; set; }
+        public static string CachedEXEStringPath { get; } = $"{CachedEXEStringDirectoryPath}EXEStringCache.json";
+        public static string CachedEXEStringDirectoryPath { get; } = "_GameCaches\\" + ProfileManager.Instance.Name + "\\";
 
+        #endregion
+
+        protected override async void OnContentRendered(EventArgs e)
+        {
             base.OnContentRendered(e);
 
+
+            //GenerateStrings();
+        }
+
+        private async Task GenerateStrings()
+        {
+            List<string> list = new();
+            if (RegistryManager.FoundExe)
+            {
+                var exeBytes = await File.ReadAllBytesAsync(RegistryManager.GamePathEXE);
+                using var nr = new NativeReader(exeBytes);
+                while(nr.Position < nr.Length)
+                {
+                    var nts = nr.ReadNullTerminatedString();
+                    if (string.IsNullOrEmpty(nts))
+                        continue;
+
+                    if (nts.Contains("\\u"))
+                        continue;
+                    
+                    if(nts.StartsWith("`"))
+                        continue;
+
+                    if (nts.Length < 3)
+                        continue;
+
+                    if (!Regex.IsMatch(nts, "([a-zA-Z])\\w+"))
+                        continue;
+
+                    list.Add(nts);
+                }
+            }
+
+            if(list.Count > 0)
+            {
+                EXEStrings = list.ToArray();
+            }
         }
 
         private void btnReset_Click(object sender, RoutedEventArgs e)
