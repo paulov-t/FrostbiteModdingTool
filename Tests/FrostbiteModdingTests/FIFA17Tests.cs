1a51b2bf5361234f577c47f7939bf46a79b35f4c
Paulov <pargsoft@hotmail.com>
2023-08-12 15:53:10 +0100

    FIFA 17 Tests - stub

---
+++ Tests/FrostbiteModdingTests/FIFA17Tests.cs
@@ -0,0 +1,64 @@
+﻿using FrostySdk.Frostbite.FIFA;
+using FrostySdk.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrostbiteModdingTests
+{
+    [TestClass]
+    public class FIFA17Tests : ILogger, IFMTTest
+    {
+        public string GamePath
+        {
+            get
+            {
+
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey("Software\\EA Sports\\FIFA 17"))
+                {
+                    if (key != null)
+                    {
+                        string installDir = key.GetValue("Install Dir").ToString();
+                        return installDir;
+                    }
+                }
+                return string.Empty;
+            }
+        }
+        public string GamePathEXE => GamePath + "FIFA17.exe";
+
+        public string GameName { get; }
+
+        public string GameEXE { get; }
+
+        public void BuildCache()
+        {
+        }
+
+        [TestMethod]    
+        public void TestAttribdbManager()
+        {
+            AttribdbManager attribdbManager = new AttribdbManager(
+                File.ReadAllBytes("G:\\Work\\FIFA Modding\\attribdb.VLT")
+                , null
+                ); ;
+        }
+
+        public void Log(string text, params object[] vars)
+        {
+        }
+
+        public void LogError(string text, params object[] vars)
+        {
+        }
+
+        public void LogWarning(string text, params object[] vars)
+        {
+        }
+    }
+}
