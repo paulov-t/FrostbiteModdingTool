44bd5bf0528a732d19d2a59ac6e72fab6daf251d
Paulov <pargsoft@hotmail.com>
2023-08-12 15:52:23 +0100

    New Feature "RegistryManager" - Stub

---
+++ Libraries/FrostySdk/FrostbiteSdk.Managers/RegistryManager.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrostySdk.FrostbiteSdk.Managers
+{
+    public static class RegistryManager
+    {
+        public static string GamePathEXE
+        {
+            get
+            {
+
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey($"Software\\EA Sports\\{ProfileManager.DisplayName}"))
+                {
+                    if (key != null)
+                    {
+                        string installDir = key.GetValue("Install Dir").ToString();
+                        installDir += $"{ProfileManager.ProfileName}.exe";
+                        if(File.Exists(installDir))
+                            return installDir;
+                    }
+                }
+
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey($"Software\\EA Games\\{ProfileManager.DisplayName}"))
+                {
+                    if (key != null)
+                    {
+                        string installDir = key.GetValue("Install Dir").ToString();
+                        installDir += $"{ProfileManager.ProfileName}.exe";
+                        if (File.Exists(installDir))
+                            return installDir;
+                    }
+                }
+                return string.Empty;
+            }
+        }
+
+        public static bool FoundExe => GamePathEXE != string.Empty;
+    }
+}
