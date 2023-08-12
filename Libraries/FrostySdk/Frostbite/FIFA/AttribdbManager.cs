a2acd3f8941cd7efaefb4c0e4831ed7f76d66805
Paulov <pargsoft@hotmail.com>
2023-08-12 15:51:26 +0100

    New Feature "AttribDbManager" - Stub

---
+++ Libraries/FrostySdk/Frostbite/FIFA/AttribdbManager.cs
@@ -0,0 +1,44 @@
+﻿using FMT.FileTools;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrostySdk.Frostbite.FIFA
+{
+    /// <summary>
+    /// An AttribDb Manager designed to read/write old FIFA Gameplay Dbs
+    /// </summary>
+    public class AttribdbManager
+    {
+        public AttribdbManager(byte[] vltBytes, byte[] binBytes) 
+        {
+            ReadVlt(vltBytes);
+            ReadBin(vltBytes);
+        }
+
+        public void ReadVlt(byte[] vltBytes)
+        {
+            if (vltBytes == null)
+                return;
+
+            if (vltBytes.Length == 0)
+                return;
+
+            using NativeReader nr = new NativeReader(vltBytes);
+        }
+
+        public void ReadBin(byte[] binBytes)
+        {
+            if (binBytes == null)
+                return;
+
+            if (binBytes.Length == 0)
+                return;
+
+            using NativeReader nr = new NativeReader(binBytes);
+        }
+    }
+}
