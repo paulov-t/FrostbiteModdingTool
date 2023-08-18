1b8696ae0ef8ff81883cb8e557faf8d342d60ffa
Paulov <pargsoft@hotmail.com>
2023-08-18 08:31:16 +0100

    Move FMT.FrostbiteGameLoader to Tools

--- Tools/FMT.FrostbiteGameLoader/Program.cs
+++ Tools/FMT.FrostbiteGameLoader/Program.cs
@@ -1,15 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Diagnostics;
 
+using var sw = File.AppendText("FMT.FrostbiteGameLoader.Log.txt");
+
 var processDirPath = AppContext.BaseDirectory;
 //Console.WriteLine($"{processDirPath}");
+sw.WriteLine(processDirPath);
 
 var processDirectoryInfo = new DirectoryInfo(processDirPath);
 var isInFIFASetup = processDirectoryInfo.FullName.Contains("FIFASetup", StringComparison.OrdinalIgnoreCase);
+sw.WriteLine("isInFIFASetup?:" + isInFIFASetup);
+
 
 var gameExeDirPath = !isInFIFASetup ? processDirPath : processDirectoryInfo.Parent?.FullName;
 if (gameExeDirPath == null)
     return;
+
+sw.WriteLine("gameExeDirPath:" + gameExeDirPath);
 
 //Console.WriteLine($"{gameExeDirPath}");
 var gameExePath = gameExeDirPath;
@@ -20,16 +27,24 @@ foreach(var f in Directory.GetFiles(gameExeDirPath))
     {
         gameExePath = f.Replace("Trial.exe", "").Replace("_", "");
         gameExePath += ".exe";
+
+
         break;
     }
 }
 
+sw.WriteLine("gameExePath:" + gameExePath);
+
 var processName = args.Length > 0 ? args[0] : gameExePath;
+
+sw.WriteLine("processName:" + processName);
+
 var programArgs = "";
 if (args.Length > 0)
     programArgs = string.Join(" ", args.Skip(1).ToArray());
 
 Console.WriteLine($"Launching {processName} {programArgs}");
+sw.WriteLine($"Launching {processName} {programArgs}");
 
 if (Process.GetProcessesByName(processName).Length > 0)
     return;
