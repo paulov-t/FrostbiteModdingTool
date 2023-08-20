4e639af352416a7bf97e4330d2d9620f9894a798
Paulov <pargsoft@hotmail.com>
2023-08-20 09:02:36 +0100

    Fix FrostbiteGameLoader to properly detect arguments sent from EADesktop

--- Tools/FMT.FrostbiteGameLoader/Program.cs
+++ Tools/FMT.FrostbiteGameLoader/Program.cs
@@ -1,63 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Diagnostics;
+﻿using System.Diagnostics;
+
+if (File.Exists("FMT.FrostbiteGameLoader.Log.txt"))
+    File.Delete("FMT.FrostbiteGameLoader.Log.txt");
 
 using var sw = File.AppendText("FMT.FrostbiteGameLoader.Log.txt");
 
-var processDirPath = AppContext.BaseDirectory;
-//Console.WriteLine($"{processDirPath}");
-sw.WriteLine(processDirPath);
-
-var processDirectoryInfo = new DirectoryInfo(processDirPath);
-var isInFIFASetup = processDirectoryInfo.FullName.Contains("FIFASetup", StringComparison.OrdinalIgnoreCase);
-sw.WriteLine("isInFIFASetup?:" + isInFIFASetup);
-
-
-var gameExeDirPath = !isInFIFASetup ? processDirPath : processDirectoryInfo.Parent?.FullName;
-if (gameExeDirPath == null)
-    return;
-
-sw.WriteLine("gameExeDirPath:" + gameExeDirPath);
-
-//Console.WriteLine($"{gameExeDirPath}");
-var gameExePath = gameExeDirPath;
-foreach(var f in Directory.GetFiles(gameExeDirPath))
+try
 {
-    //Console.WriteLine($"{f}");
-    if(f.EndsWith("Trial.exe"))
+
+    var processDirPath = AppContext.BaseDirectory;
+    //Console.WriteLine($"{processDirPath}");
+    sw.WriteLine(processDirPath);
+
+    var processDirectoryInfo = new DirectoryInfo(processDirPath);
+    var isInFIFASetup = processDirectoryInfo.FullName.Contains("FIFASetup", StringComparison.OrdinalIgnoreCase);
+    sw.WriteLine("isInFIFASetup?:" + isInFIFASetup);
+
+
+    var gameExeDirPath = !isInFIFASetup ? processDirPath : processDirectoryInfo.Parent?.FullName;
+    if (gameExeDirPath == null)
+        return;
+
+    sw.WriteLine("gameExeDirPath:" + gameExeDirPath);
+
+    //Console.WriteLine($"{gameExeDirPath}");
+    var gameExePath = gameExeDirPath;
+    foreach (var f in Directory.GetFiles(gameExeDirPath))
     {
-        gameExePath = f.Replace("Trial.exe", "").Replace("_", "");
-        gameExePath += ".exe";
+        //Console.WriteLine($"{f}");
+        if (f.EndsWith("Trial.exe"))
+        {
+            gameExePath = f.Replace("Trial.exe", "").Replace("_", "");
+            gameExePath += ".exe";
 
 
-        break;
+            break;
+        }
+    }
+
+    sw.WriteLine("gameExePath:" + gameExePath);
+
+    var processName = args.Length > 0 && args.Any(x => x.Contains(".exe")) ? args.First(x=>x.Contains(".exe")) : gameExePath;
+
+    sw.WriteLine("processName:" + processName);
+
+    var programArgs = "";
+    if (args.Length > 0)
+        programArgs = string.Join(" ", args.Where(x => !x.Contains(".exe")).ToArray());
+
+    Console.WriteLine($"Launching {processName} {programArgs}");
+    sw.WriteLine($"Launching {processName} {programArgs}");
+
+    if (Process.GetProcessesByName(processName).Length > 0)
+        return;
+
+    using (Process process = new())
+    {
+        FileInfo fileInfo = new FileInfo(processName);
+        process.StartInfo.FileName = processName;
+        process.StartInfo.WorkingDirectory = fileInfo.DirectoryName;
+        process.StartInfo.Arguments = "\"" + processName + "\"" + " " + programArgs;
+        process.StartInfo.UseShellExecute = false;
+        if (!process.Start())
+        {
+            throw new Exception($"Unable to start {processName}");
+        }
     }
 }
-
-sw.WriteLine("gameExePath:" + gameExePath);
-
-var processName = args.Length > 0 ? args[0] : gameExePath;
-
-sw.WriteLine("processName:" + processName);
-
-var programArgs = "";
-if (args.Length > 0)
-    programArgs = string.Join(" ", args.Skip(1).ToArray());
-
-Console.WriteLine($"Launching {processName} {programArgs}");
-sw.WriteLine($"Launching {processName} {programArgs}");
-
-if (Process.GetProcessesByName(processName).Length > 0)
-    return;
-
-using (Process process = new())
+catch (Exception ex)
 {
-    FileInfo fileInfo = new FileInfo(processName);
-    process.StartInfo.FileName = processName;
-    process.StartInfo.WorkingDirectory = fileInfo.DirectoryName;
-    process.StartInfo.Arguments = "\"" + processName + "\"" + " " + programArgs;
-    process.StartInfo.UseShellExecute = false;
-    if (!process.Start())
-    {
-        throw new Exception($"Unable to start {processName}");
-    }
+    sw.WriteLine(ex);
 }
