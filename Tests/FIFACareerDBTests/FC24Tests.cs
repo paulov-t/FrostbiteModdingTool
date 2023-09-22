e3585b867d110315417a79d16f4e84234413a9a7
Paulov <pargsoft@hotmail.com>
2023-09-22 22:38:15 +0100

    Add FC24 SquadFile tests

---
+++ Tests/FIFACareerDBTests/FC24Tests.cs
@@ -0,0 +1,55 @@
+﻿using FifaLibrary;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFACareerDBTests
+{
+    [TestClass]
+    public class FC24Tests : IDisposable
+    {
+        public Stream FC24TestSquadsResource
+        {
+            get
+            {
+                return FMT.FileTools.EmbeddedResourceHelper.GetEmbeddedResourceByName("FC24.Squads.Squads20230922190437");
+            }
+        }
+
+        public Stream FC24ResourceDBMeta
+        {
+            get
+            {
+                return FMT.FileTools.EmbeddedResourceHelper.GetEmbeddedResourceByName("FC24.Db.fifa_ng_db-meta.XML");
+            }
+        }
+
+
+        public Stream FC24ResourceLOCMeta
+        {
+            get
+            {
+                return FMT.FileTools.EmbeddedResourceHelper.GetEmbeddedResourceByName("FC24.Loc.eng_us-meta.XML");
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        [TestMethod]
+        public void ReadFromSquadsFile()
+        {
+            CareerFile careerFile = new CareerFile(FC24TestSquadsResource, FC24ResourceDBMeta, "");
+            if(careerFile != null)
+            {
+
+            }
+        }
+
+    }
+}
