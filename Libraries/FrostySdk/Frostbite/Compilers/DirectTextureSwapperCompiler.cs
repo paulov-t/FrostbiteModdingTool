30375cae5357251f6c1b71c2d1b7907bd0669b86
Paulov <pargsoft@hotmail.com>
2023-11-07 22:46:12 +0000

    Stub DirectTextureSwapperCompiler

---
+++ Libraries/FrostySdk/Frostbite/Compilers/DirectTextureSwapperCompiler.cs
@@ -0,0 +1,41 @@
+﻿using FMT.Logging;
+using FrostySdk.Frostbite.PluginInterfaces;
+using ModdingSupport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrostySdk.Frostbite.Compilers
+{
+    internal class DirectTextureSwapperCompiler : IAssetCompiler
+    {
+        public string ModDirectory => throw new NotImplementedException();
+
+        public bool Cleanup(FileSystem fs, ILogger logger, ModExecutor modExecuter)
+        {
+            return true;
+        }
+
+        public bool Compile(FileSystem fs, ILogger logger, ModExecutor modExecuter)
+        {
+            return true;
+        }
+
+        public bool PostCompile(FileSystem fs, ILogger logger, ModExecutor modExecuter)
+        {
+            return true;
+        }
+
+        public bool PreCompile(FileSystem fs, ILogger logger, ModExecutor modExecuter)
+        {
+            return true;
+        }
+
+        public bool RunGame(FileSystem fs, ILogger logger, ModExecutor modExecuter)
+        {
+            return true;
+        }
+    }
+}
